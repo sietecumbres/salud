4335726de13e0d3d8631c7cab4f8808e92e141ca
Juan Espinosa <juan@time4code.com>
2010-09-21 18:16:18 -0500

    project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salud::Application.config.secret_token = 'bcf4f4900ac8e8717f273158f62f30a170acbb341cb85bcb1104824aa733a696c2441047cf26a05fad9edf08f3bb486529c4e08cbb86b1eeede6392988809117'
