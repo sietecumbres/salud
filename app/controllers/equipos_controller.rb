41afb5e843108d2b2ad34c983fef20d4871ab2c8
Juan Espinosa <juan@time4code.com>
2010-09-25 15:01:27 -0500

    Search by filters

--- app/controllers/equipos_controller.rb
+++ app/controllers/equipos_controller.rb
@@ -1,24 +1,25 @@
 class EquiposController < ApplicationController
   before_filter :get_options, :only => [:index, :search]
   def index
-    
+    @busqueda = Equipo.new unless @busqueda.present?
+    logger.debug "Equipo => #{@busqueda.inspect}"
   end
   
   def search
-    t = Equipo.arel_table
-    adquisicion = params[:busqueda][:tipo_adquisicion_id].nil? ?  params[:busqueda][:otro_adquisicion] :  params[:busqueda][:tipo_adquisicion_id]
+    @busqueda = Equipo.new params[:equipo]
+    adquisicion = params[:equipo][:tipo_adquisicion_id].empty? ?  params[:otro_adquisicion] :  params[:equipo][:tipo_adquisicion_id]
     condiciones = {}
-    condiciones.merge({:area_id => params[:busqueda][:area_id]}) unless params[:busqueda][:area_id].nil?
-    condiciones.merge({:tipo_equipo_id => params[:busqueda][:tipo_equipo_id]}) unless params[:busqueda][:tipo_equipo_id].nil?
-    condiciones.merge({:subtipo_equipo_id => params[:busqueda][:subtipo_equipo_id]}) unless params[:busqueda][:subtipo_equipo_id].nil?
-    condiciones.merge({:tipo_adquisicion_id => adquisicion}) unless adquisicion.nil?
-    condiciones.merge({:responsable_id => params[:busqueda][:persona_id]}) unless params[:busqueda][:persona_id].nil?
-    condiciones.merge({:modelo => params[:busqueda][:modelo]}) unless params[:busqueda][:modelo].nil?
-    condiciones.merge({:marca => params[:busqueda][:marca]}) unless params[:busqueda][:marca].nil?
-    condiciones.merge({:serial => params[:busqueda][:serial]}) unless params[:busqueda][:serial].nil?
-    condiciones.merge({:placa => params[:busqueda][:placa]}) unless params[:busqueda][:placa].nil?
-     logger.debug "condiciones #{condiciones.inspect}"
-    @equipos_search = Equipo.where(condiciones)
+    condiciones = condiciones.merge({:area_id => params[:equipo][:area_id]}) unless params[:equipo][:area_id].empty?
+    condiciones = condiciones.merge({:tipo_equipo_id => params[:equipo][:tipo_equipo_id]}) unless params[:equipo][:tipo_equipo_id].empty?
+    condiciones = condiciones.merge({:subtipo_equipo_id => params[:equipo][:subtipo_equipo_id]}) unless params[:equipo][:subtipo_equipo_id].empty?
+    condiciones = condiciones.merge({:tipo_adquisicion_id => adquisicion}) unless adquisicion.empty?
+    condiciones = condiciones.merge({:responsable_id => params[:equipo][:responsable_id]}) unless params[:equipo][:responsable_id].empty?
+    condiciones = condiciones.merge({:modelo => params[:equipo][:modelo]}) unless params[:equipo][:modelo].empty?
+    condiciones = condiciones.merge({:marca => params[:equipo][:marca]}) unless params[:equipo][:marca].empty?
+    condiciones = condiciones.merge({:serial => params[:equipo][:serial]}) unless params[:equipo][:serial].empty?
+    condiciones = condiciones.merge({:placa => params[:equipo][:placa]}) unless params[:equipo][:placa].empty?
+     logger.debug "condiciones #{@busqueda.inspect}"
+    @equipos = Equipo.where(condiciones)
 
     render :action => :index
   end
@@ -37,7 +38,7 @@ class EquiposController < ApplicationController
       @adquisiciones = TipoAdquisicion.all
       @responsables = Persona.all
       @areas = Area.all
-       @equipos = Equipo.all
+      @equipos = Equipo.all unless @equipos.present?
       @adquisiciones << TipoAdquisicion.new({:id => '', :nombre => 'Otro'})
     end
 end
