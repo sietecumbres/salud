e91bb3c9988be1646eef793b7373d69924f6122d
Alejandro Toro Serna <alejandro@alejandro-laptop.(none)>
2010-09-25 12:47:56 -0500

    Variables for search action

--- app/controllers/equipos_controller.rb
+++ app/controllers/equipos_controller.rb
@@ -1,17 +1,17 @@
 class EquiposController < ApplicationController
+  before_filter :get_options, :only => [:index, :search]
   def index
     @equipos = Equipo.all
-    @tipos = TipoEquipo.all
-    @subtipos = SubtipoEquipo.all
-    @adquisiciones = TipoAdquisicion.all
-    @responsables = Persona.all
-    @areas = Area.all
-    
-    @adquisiciones << TipoAdquisicion.new({:id => '', :nombre => 'Otro'})
   end
   
   def search
-    
+    persona = Persona.where(:id => params[:busqueda][:persona_id])
+    tipo = TipoEquipo.where(:id => params[:busqueda][:tipo_equipo_id])
+    subtipo = SubtipoEquipo.where(:id => params[:busqueda][:subtipo_equipo_id])
+    adquisicion = TipoAdquisicion.where(:id => params[:busqueda][:tipo_adquisicion_id])
+    otro = TipoAdquisicion.where(:id => params[:busqueda][:persona_id])
+    @equipos = Equipo.all
+    render :action => :index
   end
   
   def autocomplete
@@ -20,4 +20,14 @@ class EquiposController < ApplicationController
     render :text => equipo.join(","), :layout => false
   end
 
+  protected
+    def get_options
+      @tipos = TipoEquipo.all
+      @subtipos = SubtipoEquipo.all
+      @adquisiciones = TipoAdquisicion.all
+      @responsables = Persona.all
+      @areas = Area.all
+
+      @adquisiciones << TipoAdquisicion.new({:id => '', :nombre => 'Otro'})
+    end
 end
