3602043e65d9d88f7625043fe195252bb14c5468
Marco Montes <marcomontes@gmail.com>
2010-10-12 21:50:39 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Salud::Application.routes.draw do
 
+  resources :tipo_mantenimientos
+
   match '/equipos' => 'equipos#index', :via => :get, :as => :equipos
   match '/equipo/:id/hojavida' => 'hoja_vida#show', :via => :get, :as => :hoja_vida
   match '/equipos/search' => 'equipos#search', :via => :post
