d41a82903ca39ff1a77d49a456054c0aad393bc3
Alejandro Toro Serna <alejandro@alejandro-laptop.(none)>
2010-10-19 17:02:32 -0500

    Create comparatives between programacion & ejecucion

--- app/helpers/hoja_vida_helper.rb
+++ app/helpers/hoja_vida_helper.rb
@@ -6,23 +6,43 @@ module HojaVidaHelper
 		equipo.tipos_mantenimiento.each do |tipo|
 			str << "<tr><td>#{tipo.nombre}</td><td>P</td>"
 
-			agendas_prev = equipo.agendas_for_year(year).reject{|agenda| tipo != agenda.tipo_mantenimiento}
-			fill_year(agendas_prev).each do |agenda|
-				str << "<td>#{agenda}</td>"
+			agendas_prev = equipo.agendas_for_year(year).reject{|agenda_prev| tipo != agenda_prev.tipo_mantenimiento}
+			fill_year(agendas_prev).each do |agenda_prev|
+				str << "<td>#{agenda_prev}</td>"
 			end
 
 			str << "</tr>"
 			str << "<tr><td></td><td>E</td>"
 
-			agendas_eje = equipo.agendas_for_year_eje(year).reject{|agenda| tipo != agenda.tipo_mantenimiento}
-			fill_year(agendas_eje).each do |agenda|
-				str << "<td>#{agenda}</td>"
+			agendas_eje = equipo.agendas_for_year_eje(year).reject{|agenda_eje| tipo != agenda_eje.tipo_mantenimiento}
+			fill_year_eje(agendas_eje).each do |agenda_eje|
+				str << "<td>#{agenda_eje}</td>"
 			end
 
 			str << "</tr>"
 		end
 
 		str.html_safe
+	end
+
+	def fill_year(agendas)
+		full = 1.upto(12).collect{|number| "" }
+
+		agendas.each do |agenda|
+			full[agenda.fecha_programacion.month - 1] << "<div class='agenda-day' style='background-color:#{agenda.tipo_mantenimiento.color};'>#{agenda.fecha_programacion.day}</div>"
+		end
+
+		full
+	end
+
+	def fill_year_eje(agendas)
+		full = 1.upto(12).collect{|number| "" }
+
+		agendas.each do |agenda|
+			full[agenda.fecha_ejecucion.month - 1] << "<div class='agenda-day' style='background-color:#{agenda.tipo_mantenimiento.color};'>#{agenda.fecha_ejecucion.day}</div>"
+		end
+
+		full
 	end
 
 end
