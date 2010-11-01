a8a929ac813444451c21a8e5510377958ce41b97
Juan Espinosa <juan@time4code.com>
2010-11-01 12:06:11 -0500

    agregar hojas de vida

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,138 +2,142 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 var estado_id = "";
+var datepicker_options = 	{
+														changeMonth: true,
+														changeYear: true
+													}
 
 $(document).ready(function(){
-    $(document).bind('loading.facebox', function() {
-        $(document).unbind('keydown.facebox');
-    });
-    var param =  "";
-    var data = "Core Selectors Attributes Traversing Manipulation CSS Events Effects Ajax Utilities".split(" ");
+  $(document).bind('loading.facebox', function() {
+    $(document).unbind('keydown.facebox');
+  });
+  var param =  "";
+  var data = "Core Selectors Attributes Traversing Manipulation CSS Events Effects Ajax Utilities".split(" ");
     $(".autocomplete").click(function(){
-        autocomplete_fields($(this).attr("data-remote"));
-    });
-    $(".autocomplete").focus(function(){
-        autocomplete_fields($(this).attr("data-remote"));
-    });
+      autocomplete_fields($(this).attr("data-remote"));
+  });
+  $(".autocomplete").focus(function(){
+       autocomplete_fields($(this).attr("data-remote"));
+  });
 
-    $('.datepicker').datepicker();
-    
-    $("#busqueda_tipo_adquisicion_id").change(function(){
-        if(!$(this).val()){
-            $("#otro-adquisicion").show();
-        }
-        else{
-            $("#otro-adquisicion").hide();
-        }
-    });
-    
-    $("#reporte_tipo_mantenimiento_id").change(function(){
-        if(!$(this).val()){
-            $("#otro-mantenimiento").show();
-        }
-        else{
-            $("#otro-mantenimiento").hide();
-        }
-    });
-    
-    $('a[rel*=facebox]').facebox();
-    $(".radio").click(function(){
-        estado_id = $(this).attr("data-remote");
-        $.facebox($("#div-evaluacion").val());
-    });
+  $('.datepicker').datepicker(datepicker_options);
+   
+   $("#busqueda_tipo_adquisicion_id").change(function(){
+       if(!$(this).val()){
+           $("#otro-adquisicion").show();
+       }
+       else{
+           $("#otro-adquisicion").hide();
+       }
+   });
+   
+   $("#reporte_tipo_mantenimiento_id").change(function(){
+       if(!$(this).val()){
+           $("#otro-mantenimiento").show();
+       }
+       else{
+           $("#otro-mantenimiento").hide();
+       }
+   });
+   
+   $('a[rel*=facebox]').facebox();
+   $(".radio").click(function(){
+       estado_id = $(this).attr("data-remote");
+       $.facebox($("#div-evaluacion").val());
+   });
 
-    //Funcion para adicionar la evaluacion si el estados es Reprobado
-    $("#to-eval").live('click', function(){
-        $("#estado-"+estado_id).val($("#add-eval").val());
-        var eval = $("#add-eval").val();
-        $(".rp_evaluacion").show();
-        $('#extra_eval').append(estado_id + " " + eval + "\n");
-        $(document).trigger('close.facebox');
-    });
+   //Funcion para adicionar la evaluacion si el estados es Reprobado
+   $("#to-eval").live('click', function(){
+       $("#estado-"+estado_id).val($("#add-eval").val());
+       var eval = $("#add-eval").val();
+       $(".rp_evaluacion").show();
+       $('#extra_eval').append(estado_id + " " + eval + "\n");
+       $(document).trigger('close.facebox');
+   });
 
-    //Funcion para desplegar el formulario de ingreso de repuestos
-    $(".add_repuesto").click(function(){
-        $.facebox($("#div-repuestos").val());
-    });
+   //Funcion para desplegar el formulario de ingreso de repuestos
+   $(".add_repuesto").click(function(){
+       $.facebox($("#div-repuestos").val());
+   });
 
-    //Funcion para buscar por documento de identidad
-    $("#buscar-cc").click(function(){
-        $.ajax({
-            url: "/find_by_cc",
-            data: ({documento: $("#buscar-documento").val()}),
-            type: "POST",
-            dataType: "json",
-            success: function(data){
-                if(data != "error"){
-                    $("#info-recibe").show();
-                    $("#recibe_nombre").val(data.persona.nombre);
-                    $("#recibe_cargo").val(data.persona.cargo);
-                }
-            }
-        });
-    });
+   //Funcion para buscar por documento de identidad
+   $("#buscar-cc").click(function(){
+       $.ajax({
+           url: "/find_by_cc",
+           data: ({documento: $("#buscar-documento").val()}),
+           type: "POST",
+           dataType: "json",
+           success: function(data){
+               if(data != "error"){
+                   $("#info-recibe").show();
+                   $("#recibe_nombre").val(data.persona.nombre);
+                   $("#recibe_cargo").val(data.persona.cargo);
+               }
+           }
+       });
+   });
 
-    //Funcion para mostrar los comparativos del mantenimiento
-    $("#mostrar-comparativo").live('click', function(){
-        $.ajax({
-            url: "/equipo/"+ $("#equipo").val()+"/mostrar_mantenimiento",
-            data: ({tipo_mantenimiento: $("#tipos_mantenimiento").val(), anio: $("#year").val()}),
-            type: "GET",
-            success: function(data){
-                $("#mantenimientos").html(data);
-            },
-            error: function(xhr, e, ex){
-                alert("error: "+ex);
-            }
-        });
-    });
+   //Funcion para mostrar los comparativos del mantenimiento
+   $("#mostrar-comparativo").live('click', function(){
+       $.ajax({
+           url: "/equipo/"+ $("#equipo").val()+"/mostrar_mantenimiento",
+           data: ({tipo_mantenimiento: $("#tipos_mantenimiento").val(), anio: $("#year").val()}),
+           type: "GET",
+           success: function(data){
+               $("#mantenimientos").html(data);
+           },
+           error: function(xhr, e, ex){
+               alert("error: "+ex);
+           }
+       });
+   });
 
-    //Función para almacenar el estado equipo cada que se haga clic en un radio
-    $(".r_normal").click(function(){
-        $("#estado-"+$(this).attr("data-remote")).val("");
-    });
-    
-    $("#agregar-repuesto").live('click', function(){
-        $("#tabla-repuestos").append("<tr>\
-                                        <td></td>\
-                                        <td>" + $("#ref").val() + "</td>\
-                                        <td>" + $("#cantidad").val() + "</td>\
-                                        <td>" + $("#desc").val() + "</td>\
-                                       </tr>");
-        $("#lista-repuestos").append("<input type='hidden' name='repuestos[repuesto" + $("#ref").val() + "][cant]' value='" + $("#cantidad").val() + "' />");
-        $("#lista-repuestos").append("<input type='hidden' name='repuestos[repuesto" + $("#ref").val() + "][desc]' value='" + $("#desc").val() + "' />");
-        $("#lista-repuestos").append("<input type='hidden' name='repuestos[repuesto" + $("#ref").val() + "][ref]' value='" + $("#ref").val() + "' />");
-    });
+   //Función para almacenar el estado equipo cada que se haga clic en un radio
+   $(".r_normal").click(function(){
+       $("#estado-"+$(this).attr("data-remote")).val("");
+   });
+   
+   $("#agregar-repuesto").live('click', function(){
+       $("#tabla-repuestos").append("<tr>\
+                                       <td></td>\
+                                       <td>" + $("#ref").val() + "</td>\
+                                       <td>" + $("#cantidad").val() + "</td>\
+                                       <td>" + $("#desc").val() + "</td>\
+                                      </tr>");
+       $("#lista-repuestos").append("<input type='hidden' name='repuestos[repuesto" + $("#ref").val() + "][cant]' value='" + $("#cantidad").val() + "' />");
+       $("#lista-repuestos").append("<input type='hidden' name='repuestos[repuesto" + $("#ref").val() + "][desc]' value='" + $("#desc").val() + "' />");
+       $("#lista-repuestos").append("<input type='hidden' name='repuestos[repuesto" + $("#ref").val() + "][ref]' value='" + $("#ref").val() + "' />");
+   });
 
-    $(document).bind('reveal.facebox', function(){
-        $('.datepicker').datepicker();
-        $(".autocomplete_repuestos").autocomplete({
-        source: function(req, add){
-          $.getJSON("/reportes/autocomplete_repuestos", req, function(data){
-            var suggestions = [];
-            $.each(data, function(i, val){
-              suggestions.push(val);
-            });
-          add(suggestions);
-        });},
-        minLength: 1
-      });
-    });
-    
-    $("#tipo_mantenimiento.autogenerar").live('change', function(){
-      $("#periodicidad.autogenerar").val($("#tipo_mantenimiento.autogenerar [value='" + $(this).val() + "']").attr("data-remote"));
-      $("#periodicidad.autogenerar").removeAttr('disabled');
-      $("#enviar.autogenerar").removeAttr('disabled');
-      validate_date();
-    });
+   $(document).bind('reveal.facebox', function(){
+       $('.datepicker').datepicker(datepicker_options);
+       $(".autocomplete_repuestos").autocomplete({
+       source: function(req, add){
+         $.getJSON("/reportes/autocomplete_repuestos", req, function(data){
+           var suggestions = [];
+           $.each(data, function(i, val){
+             suggestions.push(val);
+           });
+         add(suggestions);
+       });},
+       minLength: 1
+     });
+   });
+   
+   $("#tipo_mantenimiento.autogenerar").live('change', function(){
+     $("#periodicidad.autogenerar").val($("#tipo_mantenimiento.autogenerar [value='" + $(this).val() + "']").attr("data-remote"));
+     $("#periodicidad.autogenerar").removeAttr('disabled');
+     $("#enviar.autogenerar").removeAttr('disabled');
+     validate_date();
+   });
 
-    $("#year_for_agenda").change(function(){
-			var year = $(this).val();
-      $.get('/agenda/calendar', {year: year}, function(data){
-				$('#calendar').html(data);
-			});
-    });
-	
+   $("#year_for_agenda").change(function(){
+		var year = $(this).val();
+     $.get('/agenda/calendar', {year: year}, function(data){
+			$('#calendar').html(data);
+		});
+   });
+
 	$('#equipo-agenda').live('change', function(){
 		var year = $(this).val();
 		var equipo = $('#equipo-id').val();
