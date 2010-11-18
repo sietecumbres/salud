1db3e6c52ad208da8482f34e3da149cf6c8bfd68
Marco Montes <marcomontes@gmail.com>
2010-11-17 22:47:43 -0500

    Index para tipos de insumos

--- app/models/tipo_insumo.rb
+++ app/models/tipo_insumo.rb
@@ -2,6 +2,6 @@ class TipoInsumo < ActiveRecord::Base
 	has_many :tipos_insumo_pruebas
 	has_many :almacenamientos
 	has_many :insumos
-	belongs_to :fabricante
+	belongs_to :fabricante_insumo
 	belongs_to :proveedor_insumo
 end
