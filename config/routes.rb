0c4b93ab5bddbce6d1adfc4c8bad5ef78797fc34
Juan Espinosa <juan@time4code.com>
2010-11-29 18:02:00 -0500

    Se muestra informacion basica de los insumos

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Salud::Application.routes.draw do
 
   resources :proveedor_insumos
 
-	match '/kadex' => 'kardex#index', :via => :get, :as => :kardex_insumos
+	match '/kardex' => 'kardex#index', :via => :get, :as => :kardex_insumos
 	match '/kardex/:id' => 'kardex#show', :via => :get, :as => :kardex
 
   resources :fabricante_insumos
