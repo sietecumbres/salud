aeb4ba22be8d859e31b769d1cc8829f51a34a605
Juan Espinosa <juan@time4code.com>
2010-11-30 01:53:55 -0500

    Mostrar el kardex para un insumo

--- app/models/movimiento_insumo.rb
+++ app/models/movimiento_insumo.rb
@@ -4,6 +4,22 @@ class MovimientoInsumo < ActiveRecord::Base
 	belongs_to :tipo_movimiento
 	belongs_to :laboratorio
 
+	def self.get_balance(raw)
+		balance = [0]
+		self.get_transactions(raw).each do |movimiento|
+			if movimiento.tipo_movimiento_id == 1
+				balance << balance.last + movimiento.cantidad
+			else
+				balance << balance.last - movimiento.cantidad
+			end
+		end
+		balance
+	end
+	
+	def self.get_transactions(raw)
+		MovimientoInsumo.joins("join insumos as i on i.id = movimiento_insumos.insumo_id inner join tipo_insumos as ti on ti.id = i.tipo_insumo_id").where("ti.id = ?", raw)
+	end
+	
 	def self.sum_transactions(type, raw)
 		MovimientoInsumo.joins("join insumos as i on i.id = movimiento_insumos.insumo_id inner join tipo_insumos as ti on ti.id = i.tipo_insumo_id").where("ti.id = ? AND movimiento_insumos.tipo_movimiento_id = ?", raw, type).sum('cantidad')
 	end
