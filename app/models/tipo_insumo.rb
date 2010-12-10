2f516d7f1476c7c198068408f31ddfd2bd45f37d
Juan Espinosa <juan@time4code.com>
2010-12-10 01:48:36 -0500

    Agregar nombre de la clase correcta en la relacion

--- app/models/tipo_insumo.rb
+++ app/models/tipo_insumo.rb
@@ -2,7 +2,7 @@ class TipoInsumo < ActiveRecord::Base
 	has_many :tipos_insumo_pruebas
 	has_many :insumos
 	belongs_to :fabricante_insumo
-	belongs_to :proveedor_insumo
+	belongs_to :proveedor_insumo, :class_name => 'ProveedorInsumo'
 	belongs_to :alerta_cantidad, :class_name => "AlertaCantidad"
 	belongs_to :alerta_vencimiento, :class_name => "AlertaVencimiento"
 end
